db247c0ac1b8f5a1c0aa1613ed05dae0592ae90d
SomeOne768 <jalil0169@gmail.com>
2023-04-02 23:17:55 +0200

    void

--- syntaxe.sql
+++ syntaxe.sql
@@ -45,8 +45,55 @@ DECLARE
     req RECORD;
 BEGIN
     FOR req IN SELECT * FROM emp WHERE sal > 4000 AND empno != 7902 ORDER BY empno DESC LIMIT 1 LOOP
-        INSERT INTO temp VALUES (req.sal, req.ename, NULL);
+        INSERT INTO temp VALUES (NULL, req.sal, req.ename);
     END LOOP;
 END;
 $$;
+    
+
+CREATE OR REPLACE PROCEDURE createdept_az(num_dept smallint, dept_name TEXT, localisation TEXT) AS
+$$
+BEGIN
+    
+    IF num_dept NOT IN (SELECT deptno from dept) THEN
+        INSERT INTO dept VALUES (num_dept, dept_name, localisation);
+    ELSE
+        RAISE EXCEPTION 'Deja dans la table %', SQLERRM;
+    END IF;
+END;
+$$
+LANGUAGE plpgsql;
+
+
+DO $$
+DECLARE
+    jj RECORD;
+BEGIN
+    CREATE table SalIntervalle(
+        job VARCHAR(20),
+        sal_min integer,
+        sal_max integer
+    );
+
+    FOR jj IN SELECT job, min(sal) as mi, max(sal) as ma FROM emp GROUP BY job LOOP
+        INSERT INTO SalIntervalle VALUES ( jj.job, jj.mi, jj.ma);
+    END LOOP;
+END;
+$$;
+
+
+CREATE OR REPLACE FUNCTION salok_az(jobp TEXT, salaire integer) RETURNS integer AS
+$$
+DECLARE
+    mini integer := (SELECT sal_min FROM SalIntervalle as S WHERE S.job = jobp);
+    maxi integer := (SELECT sal_max FROM SalIntervalle as S WHERE S.job = jobp);
+BEGIN
+    IF salaire < mini  OR salaire > maxi THEN   
+        RETURN 0;
+    ELSE
+        RETURN 1;
+    END IF;
+END;
+$$
+LANGUAGE plpgsql;
     
